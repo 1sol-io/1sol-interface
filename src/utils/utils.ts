28bd97666b854691a05c2e1322bafaa7f85a0527
Sebastian Conybeare <sebastian@projectserum.com>
2020-10-30 21:43:54 +0800

    Don't prepend 0x to base58 addresses, and change the format of unrecognized tokens

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -53,9 +53,9 @@ export function useLocalStorageState(key: string, defaultState?: string) {
   return [state, setLocalStorageState];
 }
 
-// shorten the checksummed version of the input address to have 0x + 4 characters at start and end
+// shorten the checksummed version of the input address to have 4 characters at start and end
 export function shortenAddress(address: string, chars = 4): string {
-  return `0x${address.substring(0, chars)}...${address.substring(44 - chars)}`;
+  return `${address.substring(0, chars)}...${address.substring(44 - chars)}`;
 }
 
 export function getTokenName(env: ENV, mintAddress: string): string {
@@ -64,7 +64,7 @@ export function getTokenName(env: ENV, mintAddress: string): string {
     return knownSymbol;
   }
 
-  return shortenAddress(mintAddress).substring(10).toUpperCase();
+  return `${mintAddress.substring(0, 5)}...`;
 }
 
 export function getTokenIcon(
