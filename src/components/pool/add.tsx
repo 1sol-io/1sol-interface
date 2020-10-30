7ada81865ab556ed2e83aef1d1fa3ad419c707dd
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2020-10-29 20:34:15 -0500

    fix: label for adding liquidity

--- src/components/pool/add.tsx
+++ src/components/pool/add.tsx
@@ -46,37 +46,37 @@ export const AddToLiquidity = () => {
   const executeAction = !connected
     ? wallet.connect
     : async () => {
-        if (A.account && B.account && A.mint && B.mint) {
-          setPendingTx(true);
-          const components = [
-            {
-              account: A.account,
-              mintAddress: A.mintAddress,
-              amount: A.convertAmount(),
-            },
-            {
-              account: B.account,
-              mintAddress: B.mintAddress,
-              amount: B.convertAmount(),
-            },
-          ];
+      if (A.account && B.account && A.mint && B.mint) {
+        setPendingTx(true);
+        const components = [
+          {
+            account: A.account,
+            mintAddress: A.mintAddress,
+            amount: A.convertAmount(),
+          },
+          {
+            account: B.account,
+            mintAddress: B.mintAddress,
+            amount: B.convertAmount(),
+          },
+        ];
 
-          addLiquidity(connection, wallet, components, slippage, pool, options)
-            .then(() => {
-              setPendingTx(false);
-            })
-            .catch((e) => {
-              console.log("Transaction failed", e);
-              notify({
-                description:
-                  "Please try again and approve transactions from your wallet",
-                message: "Adding liquidity cancelled.",
-                type: "error",
-              });
-              setPendingTx(false);
+        addLiquidity(connection, wallet, components, slippage, pool, options)
+          .then(() => {
+            setPendingTx(false);
+          })
+          .catch((e) => {
+            console.log("Transaction failed", e);
+            notify({
+              description:
+                "Please try again and approve transactions from your wallet",
+              message: "Adding liquidity cancelled.",
+              type: "error",
             });
-        }
-      };
+            setPendingTx(false);
+          });
+      }
+    };
 
   const hasSufficientBalance = A.sufficientBalance() && B.sufficientBalance();
 
@@ -91,25 +91,25 @@ export const AddToLiquidity = () => {
       type="primary"
       size="large"
     >
-      {generateActionLabel(ADD_LIQUIDITY_LABEL, connected, env, A, B)}
+      {generateActionLabel(CREATE_POOL_LABEL, connected, env, A, B)}
       {pendingTx && <Spin indicator={antIcon} className="add-spinner" />}
     </Button>
   ) : (
-    <Dropdown.Button
-      className="add-button"
-      onClick={executeAction}
-      disabled={
-        connected &&
-        (pendingTx || !A.account || !B.account || A.account === B.account)
-      }
-      type="primary"
-      size="large"
-      overlay={<PoolConfigCard options={options} setOptions={setOptions} />}
-    >
-      {generateActionLabel(CREATE_POOL_LABEL, connected, env, A, B)}
-      {pendingTx && <Spin indicator={antIcon} className="add-spinner" />}
-    </Dropdown.Button>
-  );
+      <Dropdown.Button
+        className="add-button"
+        onClick={executeAction}
+        disabled={
+          connected &&
+          (pendingTx || !A.account || !B.account || A.account === B.account)
+        }
+        type="primary"
+        size="large"
+        overlay={<PoolConfigCard options={options} setOptions={setOptions} />}
+      >
+        {generateActionLabel(CREATE_POOL_LABEL, connected, env, A, B)}
+        {pendingTx && <Spin indicator={antIcon} className="add-spinner" />}
+      </Dropdown.Button>
+    );
 
   return (
     <div>
@@ -176,7 +176,7 @@ export const AddToLiquidity = () => {
               !hasSufficientBalance)
           }
         >
-          {generateActionLabel(CREATE_POOL_LABEL, connected, env, A, B)}
+          {generateActionLabel(ADD_LIQUIDITY_LABEL, connected, env, A, B)}
           {pendingTx && <Spin indicator={antIcon} className="add-spinner" />}
         </Button>
       )}
