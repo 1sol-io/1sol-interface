10ccc5b53d16ba83e4f0eb1fcb045ebe4fc307f6
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2020-10-31 09:02:44 -0500

    feat: move markets to context

---
+++ src/context/market.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+
+export interface MarketsContextState {
+
+}
+
+const MarketsContext = React.createContext<MarketsContextState | null>(
+  null
+);
+
+export function MarketProvider({ children = null as any }) {
+
+
+  return (
+    <MarketsContext.Provider
+      value={{
+      }}
+    >
+      {children}
+    </MarketsContext.Provider>
+  );
+}
+
+export const useMarkets = () => {
+  const context = useContext(MarketsContext);
+  return context as MarketsContextState;
+};
