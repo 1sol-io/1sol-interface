ff5e0b3b3f1ae87a6e5bd64abd0931d7d3545995
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2020-11-02 10:56:29 -0600

    feat: remove TOMO override

--- src/models/marketOverrides.ts
+++ src/models/marketOverrides.ts
@@ -1,4 +1,4 @@
+
+// use to override serum market to use specifc mint
 export const MINT_TO_MARKET: { [key: string]: string } = {
-  GXMvfY2jpQctDqZ9RoU3oWPhufKiCcFEfchvYumtX7jd:
-    "B1GypajMh7S8zJVp6M1xMfu6zGsMgvYrt3cSn9wG7Dd6", // TOMO market override
 };
