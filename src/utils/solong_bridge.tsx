debe365c3268ba6444a37e9a53785848613bb3bc
CZ <cz.theng@gamil.com>
2020-11-07 20:50:16 +0800

    Add support for solong wallet extension

---
+++ src/utils/solong_bridge.tsx
@@ -0,0 +1,38 @@
+import EventEmitter from 'eventemitter3';
+import { PublicKey } from '@solana/web3.js';
+
+export class SolongBridge extends EventEmitter {
+  _publicKey: any
+  _onProcess:boolean
+  constructor(providerUrl: string, network: string) {
+    super()
+    this._publicKey = null;
+    this._onProcess = false;
+    this.connect= this.connect.bind(this);
+  }
+
+  get publicKey() {
+    return this._publicKey;
+  }
+
+  async signTransaction(transaction: any) {
+    return (window as any).solong.signTransaction(transaction);
+  }
+
+  connect() {
+    if (this._onProcess) {
+      return ;
+    }
+    this._onProcess = true;
+    console.log('solong helper select account');
+    (window as any).solong
+      .selectAccount()
+      .then((account: any) => {
+        this._publicKey = new PublicKey(account);
+        console.log('window solong select:', account, 'this:', this);
+        this.emit('connect', this._publicKey);
+      })
+      .catch(() => {})
+      .finally(()=>{this._onProcess=false});
+  }
+}
--- src/utils/wallet.tsx
+++ src/utils/wallet.tsx
@@ -3,10 +3,12 @@ import Wallet from "@project-serum/sol-wallet-adapter";
 import { notify } from "./notifications";
 import { useConnectionConfig } from "./connection";
 import { useLocalStorageState } from "./utils";
+import {SolongBridge} from "./solong_bridge";
 
 export const WALLET_PROVIDERS = [
   { name: "sollet.io", url: "https://www.sollet.io" },
   { name: "solflare.com", url: "https://solflare.com/access-wallet" },
+  { name: "solongwallet.com", url: "http://solongwallet.com" },
 ];
 
 const WalletContext = React.createContext<any>(null);
@@ -17,7 +19,13 @@ export function WalletProvider({ children = null as any }) {
     "walletProvider",
     "https://www.sollet.io"
   );
-  const wallet = useMemo(() => new Wallet(providerUrl, endpoint), [
+  const wallet = useMemo(() => {
+    console.log("use new provider:", providerUrl, " endpoint:", endpoint)
+    if (providerUrl==="http://solongwallet.com")  {
+      return new SolongBridge(providerUrl, endpoint);
+    } else {
+      return new Wallet(providerUrl, endpoint)
+    }}, [
     providerUrl,
     endpoint,
   ]);
