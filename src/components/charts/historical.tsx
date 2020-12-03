917039b6cc6dcbff177f15059bad5cd339ced5d7
juan <juandgoc@gmail.com>
2020-12-03 12:15:07 -0500

    added base component for chart

--- src/components/charts/historical.tsx
+++ src/components/charts/historical.tsx
@@ -6,10 +6,18 @@ import { PoolInfo } from "../../models";
 import { PoolIcon } from "../tokenIcon";
 import { formatShortDate, getPoolName } from "../../utils/utils";
 import { useConnectionConfig } from "../../utils/connection";
+import { BONFIDA_POOL_INTERVAL } from "../../context/market";
 
 export const VOLUME_API = "https://serum-api.bonfida.com/pools/volumes";
 export const LIQUIDITY_API = "https://serum-api.bonfida.com/pools/liquidity";
 
+const API_ENDPOINTS: EndpointOptions = {
+  volume: VOLUME_API,
+  liquidity: LIQUIDITY_API
+}
+type EndpointOptions = {
+  [key: string]: string
+}
 interface VolumeData {
   volume: number;
   time: number;
@@ -20,6 +28,115 @@ interface LiquidityData {
   liquidityBinUsd: number;
   time: number;
 }
+
+export const PoolLineChart = React.memo((props:
+  {
+    pool?: PoolInfo;
+    limit?: number;
+    api: string;
+    chartName: string;
+    current?: number;
+  }) => {
+    const { pool, api, limit, chartName, current } = props;
+    const chartDiv = useRef<HTMLDivElement>(null);
+    const echartsRef = useRef<any>(null);
+
+    let apiFilter: string = "";
+    let apiUrl: string = "";
+    if (pool) {
+      const baseMintAddress = pool.pubkeys.holdingMints[0].toBase58();
+      const quoteMintAddress = pool.pubkeys.holdingMints[1].toBase58();
+      apiFilter = `?mintA=${baseMintAddress}&mintB=${quoteMintAddress}`
+    }
+    apiUrl = API_ENDPOINTS[api];
+
+    const bonfidaDataChartQuery = async () => {
+      try {
+        const resp = await window.fetch(
+          `${apiUrl}${apiFilter}`
+        );
+        const data = await resp.json();
+        let finalData = data?.data || [];
+        if (limit && finalData) {
+          finalData = finalData.slice(0, limit);
+        }
+
+        updateChart(finalData);
+      } catch {
+        // ignore
+      }
+    };
+
+    const updateChart = (data: any) => {
+      if (echartsRef.current) {
+        echartsRef.current.setOption({
+          title: {
+            text: `Historical ${chartName} ${current}`,
+            color: "#fff",
+            textStyle: {
+              color: "#fff",
+            },
+          },
+          textStyle: {
+            color: "#fff",
+          },
+          tooltip: {
+            trigger: "axis",
+            axisPointer: {
+              type: "shadow",
+            },
+          },
+          grid: {
+            containLabel: true,
+            left: 0,
+            right: 0,
+          },
+          xAxis: [
+            {
+              inverse: true,
+              name: "Date",
+              nameLocation: "middle",
+              type: "category",
+              data: data.map((d: any) =>
+                formatShortDate.format(new Date(d.time))
+              ),
+            },
+          ],
+          yAxis: [
+            {
+              name: chartName,
+              type: "value",
+              scale: true,
+              splitLine: false,
+            },
+          ],
+          series: [
+            {
+              type: "line",
+              data: data.map((d: any) => d[api]),
+            },
+          ],
+        });
+      }
+    };
+    useEffect(() => {
+      if (chartDiv.current) {
+        echartsRef.current = echarts.init(chartDiv.current);
+      }
+      bonfidaDataChartQuery();
+      let bonfidaTimer = 0;
+      bonfidaTimer = window.setInterval(
+        () => bonfidaDataChartQuery(),
+        BONFIDA_POOL_INTERVAL
+      );
+      return () => {
+        echartsRef.current.dispose();
+        window.clearInterval(bonfidaTimer)
+      };
+    });
+    return <div ref={chartDiv} style={{ height: "250px", width: "100%" }} />;;
+  }
+)
 
 export const HistoricalVolume = React.memo(
   (props: { pool: PoolInfo; poolName?: any }) => {
