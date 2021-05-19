2905b31036d7a8f3f5610e58d5d470b0a4e10aac
gaowhen <gaowhen.com@gmail.com>
2021-05-19 12:47:31 +0800

    Revert "Set max slippage to 25%"
    This reverts commit abb6720a13da565cdb6477eecbac01ff2d1cd0e8.

--- src/components/slippage/index.tsx
+++ src/components/slippage/index.tsx
@@ -3,9 +3,6 @@ import { Button } from "antd";
 import { useSlippageConfig } from "./../../utils/connection";
 import { NumericInput } from "./../numericInput";
 
-const MAX_SLIPPAGE = 25.0
-const DEFAULT_MIN_SLIPPAGE = 0.1
-
 export const Slippage = () => {
   const { slippage, setSlippage } = useSlippageConfig();
   const slippagePct = slippage * 100;
@@ -53,10 +50,8 @@ export const Slippage = () => {
             outline: "transpaernt",
           }}
           onChange={(x: any) => {
-            const cappedSlippage = Math.min(parseFloat(x), MAX_SLIPPAGE);
-            const safeCappedSlippage = Number.isNaN(cappedSlippage) ? DEFAULT_MIN_SLIPPAGE.toString() : cappedSlippage.toString();
-            setValue(safeCappedSlippage);
-            const newValue = parseFloat(safeCappedSlippage) / 100.0;
+            setValue(x);
+            const newValue = parseFloat(x) / 100.0;
             if (Number.isFinite(newValue)) {
               setSlippage(newValue);
             }
