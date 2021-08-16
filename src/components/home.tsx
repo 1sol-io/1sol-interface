7b3a3fe1ab17694b79f8dab1dccfca02798ec0b9
gaowhen <gaowhen.com@gmail.com>
2021-08-16 16:44:03 +0800

    feat: ga

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,8 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'antd'
 
-import React, {useEffect} from "react";
-import { Link } from "react-router-dom";
-import { Button } from "antd";
-
-import { AppBar } from "./appBar";
+import { AppBar } from './appBar'
 
 import twitter from '../assets/twitter.svg'
 import telegram from '../assets/telegram.svg'
@@ -24,33 +23,47 @@ export const HomePage = () => {
         <div className="hd">
           <h1>An innovative cross-chain DeFi aggregator on Solana and more</h1>
         </div>
-        <div className="bd">
-        </div>
+        <div className="bd" />
         <div className="ft">
           <div className="socials">
             <div className="flex">
               <div className="icon">
-                <a href="https://twitter.com/1solprotocol"><img src={twitter} /></a>
+                <a href="https://twitter.com/1solprotocol">
+                  <img src={twitter} />
+                </a>
               </div>
               <div className="icon">
-                <a href="https://t.me/onesolcommunity"><img src={telegram} /></a>
+                <a href="https://t.me/onesolcommunity">
+                  <img src={telegram} />
+                </a>
               </div>
               <div className="icon">
-                <a href="https://medium.com/@1solProtocol"><img src={medium} /></a>
+                <a href="https://medium.com/@1solProtocol">
+                  <img src={medium} />
+                </a>
               </div>
               <div className="icon">
-                <a href="https://github.com/1sol-io"><img src={github} /></a>
+                <a href="https://github.com/1sol-io">
+                  <img src={github} />
+                </a>
               </div>
             </div>
           </div>
-          <div className="powerby">Built on: <img src={solana} /><a href="https://solana.com">Solana</a></div>
+          <div className="powerby">
+            Built on: <img src={solana} />
+            <a href="https://solana.com">Solana</a>
+          </div>
           <div className="buttons">
             <div className="flex btn-flex">
-              <a href="https://file.1sol.io/static/1SolProtocolLightPaper.pdf">
-                <Button className="custom-btn purple-btn" type="text" size="large">Download LightPaper</Button>
+              <a href="https://file.1sol.io/static/1Sol%20Protocol%20Pitch%20Deck%20v0.6.pdf">
+                <Button className="custom-btn purple-btn" type="text" size="large">
+                  Download LightPaper
+                </Button>
               </a>
-              <Link to={{pathname: '/trade'}}>
-                <Button className="custom-btn purple-btn-outline" type="text" size="large">Trade(devnet)</Button>
+              <Link to={{ pathname: '/trade' }}>
+                <Button className="custom-btn purple-btn-outline" type="text" size="large">
+                  Trade(devnet)
+                </Button>
               </Link>
             </div>
           </div>
