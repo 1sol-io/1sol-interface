4a5fdb7f76a15cfa8f013249ae09d6c84010e864
gaowhen <gaowhen.com@gmail.com>
2021-11-30 13:04:49 +0800

    feat: airdrop page

---
+++ src/components/airdrop/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Airdrop = () => {
+  return (
+    <div className="airdrop">
+      <div className="hd">
+        <h2>Airdrop</h2>
+      </div>
+      <div className="bd" />
+    </div>
+  )
+}
+
+export default Airdrop
