d107d0abb33b9233239936b6cc5aae6ef574a9d8
gaowhen <gaowhen.com@gmail.com>
2021-12-10 14:02:05 +0800

    feat: show loading before ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import React from "react";
-import { Routes } from "./routes";
+import React, { useEffect, useState } from 'react'
+import { Routes } from './routes'
 
-import "./App.less";
+import Logo from './assets/logo.png'
 
-function App() {
+import './App.less'
+
+function App(){
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    const checkStatus = () => {
+      if (document.readyState === 'complete') {
+        setReady(true)
+      } else {
+        const timer = setTimeout(checkStatus, 100)
+
+        return () => {
+          clearTimeout(timer)
+        }
+      }
+    }
+
+    checkStatus()
+  }, [])
+
   return (
     <div className="App">
-      <Routes />
+      {ready ? (
+        <Routes />
+      ) : (
+        <div className="g-loading">
+          <img className="g-loading-logo" src={Logo} alt="logo" />
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
