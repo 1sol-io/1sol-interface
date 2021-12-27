950b2a756b86247672137b7480aa64ec573eea04
gaowhen <gaowhen.com@gmail.com>
2021-12-27 15:13:33 +0800

    chore: sentry;

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+import './index.css'
+
+Sentry.init({
+  dsn:
+    'https://b38fa4cb18f34d3fadb15787420bab8c@o288719.ingest.sentry.io/6124533',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 0.1
+})
 
 ReactDOM.render(
   <React.StrictMode>
